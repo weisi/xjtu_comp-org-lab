32f6d7d6f908d98696f1f3b211d1fbf8358078b9
Weisi Dai <multiple1902@gmail.com>
2012-06-23 00:06:58 +0800

    improved chartable generator script

--- brainfuck-machine/tools/chartable.py
+++ brainfuck-machine/tools/chartable.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
+chars = []
+chars += range(ord('a'), ord('z') + 1, 1) 
+chars += range(ord('A'), ord('Z') + 1, 1) 
+chars += range(ord('0'), ord('9') + 1, 1)
 
-for i in range(ord('a'), ord('z') + 1, 1) + range(ord('A'), ord('Z') + 1, 1) + range(ord('0'), ord('9') + 1, 1):
+for i in chars:
     print "when %s=>" % i
     print "    write(l, string'(\"%s\"));" % (chr(i))
